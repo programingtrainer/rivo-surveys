0e8f17aca1d01287b1e7965fa02aa4e9fc7be743
programingtrainer <programingtrainer2@gmail.com>
2026-09-09 11:53:30 +0300

    Disable global router refresh on Cloudflare

--- app/LiveRefresh.tsx
+++ app/LiveRefresh.tsx
@@ -1,35 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
-const INTERVAL = 3000;
-
 export default function LiveRefresh() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const refresh = () => {
-      if (document.visibilityState === "visible") {
-        router.refresh();
-      }
-    };
-
-    const timer = setInterval(refresh, INTERVAL);
-
-    const handleVisibility = () => {
-      if (document.visibilityState === "visible") {
-        router.refresh();
-      }
-    };
-
-    document.addEventListener("visibilitychange", handleVisibility);
-
-    return () => {
-      clearInterval(timer);
-      document.removeEventListener("visibilitychange", handleVisibility);
-    };
-  }, [router]);
-
   return null;
 }
